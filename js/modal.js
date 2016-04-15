02a903a2c4e6c7088451dff3cf109a4188ba7208
lunacy <lunacy@lunacyui-MacBook-Air.local>
2016-04-15 16:03:01 +0900

    5tardium

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+var modal = document.getElementById("tickets_Modal");
+var click = document.getElementsByClassName("ct_Ticket")[0];
+var clo = document.getElementsByClassName("close")[0];
+click.onclick = function(){
+    modal.style.display = "block";
+}
+clo.onclick = function(){
+    modal.style.display = "none";
+}
+var modal1 = document.getElementById("lounge_Modal");
+var click1 = document.getElementsByClassName("ct_Lounge")[0];
+var clo1 = document.getElementsByClassName("close")[1];
+click1.onclick = function(){
+    modal1.style.display = "block";
+}
+clo1.onclick = function(){
+    modal1.style.display = "none";
+}
+
+var modal2 = document.getElementById("contact_Modal");
+var click2 = document.getElementsByClassName("ct_Contact")[0];
+var clo2 = document.getElementsByClassName("close")[2];
+click2.onclick = function(){
+    modal2.style.display = "block";
+}
+clo2.onclick = function(){
+    modal2.style.display = "none";
+}
