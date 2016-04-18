9ad6fdde434976bc7a8472fc65977712d88b0702
lunacy <lunacy@lunacyui-MacBook-Air.local>
2016-04-18 16:06:24 +0900

    fix

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,8 @@ var click = document.getElementsByClassName("ct_Ticket")[0];
 var clo = document.getElementsByClassName("close")[0];
 click.onclick = function(){
     modal.style.display = "block";
+     modal1.style.display = "none";
+    modal2.style.display = "none";
 }
 clo.onclick = function(){
     modal.style.display = "none";
@@ -12,9 +14,12 @@ var click1 = document.getElementsByClassName("ct_Lounge")[0];
 var clo1 = document.getElementsByClassName("close")[1];
 click1.onclick = function(){
     modal1.style.display = "block";
+    modal.style.display = "none";
+    modal2.style.display = "none";
 }
 clo1.onclick = function(){
     modal1.style.display = "none";
+    
 }
 
 var modal2 = document.getElementById("contact_Modal");
@@ -22,7 +27,11 @@ var click2 = document.getElementsByClassName("ct_Contact")[0];
 var clo2 = document.getElementsByClassName("close")[2];
 click2.onclick = function(){
     modal2.style.display = "block";
+     modal.style.display = "none";
+    modal2.style.display = "none";
+    
 }
 clo2.onclick = function(){
     modal2.style.display = "none";
+    
 }
